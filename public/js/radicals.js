736c9ff717793cb9c9ab7eb5bc891966b73fb703
NxReason <arsiddde@gmail.com>
2025-01-21 07:08:43 +0300

    add: radicals modal & new radical ui

--- public/js/radicals.js
+++ public/js/radicals.js
@@ -11,6 +11,18 @@ const API = {
         return null;
       }
     },
+
+    async save(body) {
+      const headers = {
+        method: 'POST',
+        'Content-Type': 'application/json',
+        body: JSON.stringify(body),
+      };
+
+      const res = await fetch(this.url, headers);
+      const json = await res.json();
+      return json.radical;
+    },
   },
 };
 
@@ -19,7 +31,17 @@ const Radicals = {
     this.$container = document.getElementById('radicals-table-body');
     this.radicals = await API.radicals.all();
     this.populateTable();
+
+    this.newButton = document.getElementById('btn-new-radical');
+    this.setEventHandlers();
   },
+
+  add(radical) {
+    this.radicals.push(radical);
+    const row = this.createRow(radical);
+    this.$container.append(row);
+  },
+
   populateTable() {
     const rows = this.radicals.map(this.createRow);
     this.$container.append(...rows);
@@ -47,6 +69,54 @@ const Radicals = {
     row.append(glyph, meanings, progress, controls);
     return row;
   },
+
+  setEventHandlers() {
+    this.newButton.addEventListener('click', () => {
+      Modal.open();
+    });
+  },
+};
+
+const Modal = {
+  init() {
+    this.container = document.getElementById('modal-radical');
+
+    this.form = document.getElementById('form-radical');
+    this.glyphInput = document.getElementById('input-radical-glyph');
+    this.namesInput = document.getElementById('input-radical-names');
+    this.saveBtn = document.getElementById('btn-save-radical');
+    this.closeBtn = document.getElementById('btn-close-modal');
+    this.setFormHandlers();
+  },
+
+  setFormHandlers() {
+    this.form.addEventListener('submit', async e => {
+      e.preventDefault();
+      const radicalData = this.collectData();
+      const radical = await API.radicals.save(radicalData);
+      Radicals.add(radical);
+      this.close();
+    });
+    this.closeBtn.addEventListener('click', e => {
+      e.stopPropagation();
+      e.preventDefault();
+      this.close();
+    });
+  },
+
+  open() {
+    this.container.style.display = 'block';
+  },
+  close() {
+    this.container.style.display = 'none';
+  },
+
+  collectData() {
+    const glyph = this.glyphInput.value;
+    let meanings = this.namesInput.value.split(',').map(m => m.trim());
+    return { glyph, meanings };
+  },
 };
 
 Radicals.init();
+Modal.init();
